337251467c0d1bb44503690ace27525c61d87be0
Marcospantunes <marcospantunes203@gmail.com>
2023-11-18 13:11:18 -0300

    Projeto criado

---
+++ scripts/animation.js
@@ -0,0 +1,52 @@
+let img = new Image();
+img.src = '../assets/duckySpriteAndando.png';
+img.onload = function() {
+    init();
+}
+
+let canvas = document.querySelector('canvas');
+let contexto = canvas.getContext('2d');
+const escala = 5.5;
+const largura = 32;
+const altura = 42;
+const escalaLargura = escala * largura;
+const escalaAltura = escala * altura;
+
+function desenhaQuadro(posX, posY, canvasX, canvasY) {
+    contexto.drawImage(img, posX*largura, posY*altura, largura, altura, canvasX, canvasY, escalaLargura, escalaAltura);
+}
+
+let imagens = [0, 1, 2, 3, 4, 5, 0];
+let indiceImagem = 0;
+let contaQuadro = 0;
+let direcaoAtual = 0;
+
+function passo() {
+    contaQuadro++;
+    if(contaQuadro < 10) {
+        window.requestAnimationFrame(passo);
+        return;
+    }
+
+    contaQuadro = 0;
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+    desenhaQuadro(imagens[indiceImagem], direcaoAtual, escalaLargura - 120 , -42);
+    indiceImagem++;
+    if(indiceImagem >= imagens.length){
+        indiceImagem = 0;
+        direcaoAtual++;
+    }
+        if(direcaoAtual >= 2) {
+            direcaoAtual = 1;
+        } 
+   
+    window.requestAnimationFrame(passo);
+}
+
+function init() {
+    window.requestAnimationFrame(passo);
+}
+
+setTimeout(() => {
+    img.src = '../assets/duckySpriteIdle.png';
+}, 5000)
---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+const numeroAleatorio = Math.floor(Math.random() * 1000 + 1)
+const numeroDoUsuario = document.querySelector('#numeroDoUsuario')
+const chutar = document.querySelector('#chutar')
+const jogarNovamente = document.querySelector('#jogarNovamente')
+const balao = document.querySelector('.balao')
+const areaAnimacao = document.querySelector('#areaDaAnimacao')
+const audioPato = document.querySelector('#audioPato')
+const logo = document.querySelector('#logo')
+const audioLogo = document.querySelector('#audioPatoLogo')
+
+areaAnimacao.addEventListener('click', () => {
+    audioPato.play()
+})
+
+logo.addEventListener('click', () => {
+    audioLogo.play()
+})
+
+setTimeout(() => {
+    numeroDoUsuario.removeAttribute('disabled')
+    balao.style.display = 'block'
+}, 5500)
+
+function verificaNumero() {
+    if(numeroDoUsuario.value != "") {
+        chutar.removeAttribute('disabled')
+        
+        chutar.addEventListener('click', () => {
+            console.log(numeroDoUsuario.value)
+            if(numeroDoUsuario.value == numeroAleatorio) {
+                balao.innerHTML = 'Acertou!'
+                balao.style.marginLeft = '120px'
+
+                audioPato.src = '/assets/audio/quack-music.mp3'
+                audioPato.play()
+                chutar.setAttribute('disabled' ,'true')
+                jogarNovamente.style.display = 'block'
+
+            }
+
+            if(numeroDoUsuario.value > numeroAleatorio) {
+                balao.innerHTML = `O número é menor que ${numeroDoUsuario.value}!`
+            } else if(numeroDoUsuario.value < numeroAleatorio) [
+                balao.innerHTML = `O número é maior que ${numeroDoUsuario.value}!`
+            ]
+        }
+        )
+    } else {
+        chutar.setAttribute('disabled' ,'true')
+    }
+}
+
+function restart() {
+    window.location.reload()
+}
